f7cd3ccbc3503ce75dec8c07e9aa1ae1af93f144
AyoBakre <ayobakre@gmail.com>
2021-04-21 22:11:46 +0100

    fixed bugs

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -318,10 +318,3 @@ var play = function(event){
   }
 }
 
-var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-if (!isChrome){
-    $('#iframeAudio').remove()
-}
-else {
-    $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
-}
